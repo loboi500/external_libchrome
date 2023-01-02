c6a3c4138bb3f8c9de49b69f4d1af14c6311e5e6
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 21:59:00 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- testrunner.cc
+++ testrunner.cc
@@ -1,28 +1,3 @@
-/*
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-#include <gtest/gtest.h>
-
-#include "base/at_exit.h"
-#include "base/command_line.h"
-
-int main(int argc, char** argv) {
-  base::AtExitManager at_exit_manager;
-  base::CommandLine::Init(argc, argv);
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2b08dea2c5da5a9fd86e291df628f60ad1b6c73372bfb9d3642b9fad9c6f249
+size 888
